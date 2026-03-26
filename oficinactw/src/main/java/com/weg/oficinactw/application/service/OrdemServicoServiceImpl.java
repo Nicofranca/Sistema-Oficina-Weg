429c55b5555d1d62f2a41d19862c63e292cad8c7
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-26 16:27:11 -0300

    feat: implementa os metodos da interface no TurmaServiceImpl

--- oficinactw/src/main/java/com/weg/oficinactw/application/service/OrdemServicoServiceImpl.java
+++ oficinactw/src/main/java/com/weg/oficinactw/application/service/OrdemServicoServiceImpl.java
@@ -1,4 +1,30 @@
 package com.weg.oficinactw.application.service;
 
-public class OrdemServicoServiceImpl {
+import com.weg.oficinactw.application.dto.ordemServico.OrdemServicoResponseDTO;
+import com.weg.oficinactw.domain.service.OrdemServicoService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrdemServicoServiceImpl implements OrdemServicoService {
+    @Override
+    public OrdemServicoResponseDTO abrirOrdemServico(Long professorId, List<Long> alunosId, String equipamento, String defeito) {
+        return null;
+    }
+
+    @Override
+    public OrdemServicoResponseDTO registrarExecucao(Long ordemServicoId, Long alunoId, String materiais, String laudo) {
+        return null;
+    }
+
+    @Override
+    public OrdemServicoResponseDTO finalizarOrdemServico(Long ordemServicoId, Long professorId) {
+        return null;
+    }
+
+    @Override
+    public List<OrdemServicoResponseDTO> listarOSAtivas() {
+        return List.of();
+    }
 }
--- oficinactw/src/main/java/com/weg/oficinactw/domain/service/OrdemServicoService.java
+++ oficinactw/src/main/java/com/weg/oficinactw/domain/service/OrdemServicoService.java
@@ -1,4 +1,13 @@
 package com.weg.oficinactw.domain.service;
 
+import com.weg.oficinactw.application.dto.ordemServico.OrdemServicoResponseDTO;
+
+import java.util.List;
+
 public interface OrdemServicoService {
+    OrdemServicoResponseDTO abrirOrdemServico (Long professorId, List<Long> alunosId, String equipamento, String defeito);
+    OrdemServicoResponseDTO registrarExecucao (Long ordemServicoId, Long alunoId, String materiais, String laudo);
+    OrdemServicoResponseDTO finalizarOrdemServico (Long ordemServicoId, Long professorId);
+    List<OrdemServicoResponseDTO> listarOSAtivas (); //Retorna apenas as que são concluidas
 }
+
