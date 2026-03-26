f98ed8f4a80faf6ad8b8578770aac2de7e11a2d8
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-26 14:08:05 -0300

    feat: inicia versionamento ao projeto

---
+++ oficinactw/src/main/java/com/weg/oficinactw/OficinactwApplication.java
@@ -0,0 +1,13 @@
+package com.weg.oficinactw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinactwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinactwApplication.class, args);
+	}
+
+}
---
+++ oficinactw/src/test/java/com/weg/oficinactw/OficinactwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.oficinactw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinactwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
