31a95e70c1d7812fd6f0869f1323ad2b94fca934
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-26 20:51:31 -0300

    feat: implementacao dos metodos de TurmaServiceImpl

--- oficinactw/src/main/java/com/weg/oficinactw/application/service/TurmaServiceImpl.java
+++ oficinactw/src/main/java/com/weg/oficinactw/application/service/TurmaServiceImpl.java
@@ -3,8 +3,12 @@ package com.weg.oficinactw.application.service;
 import com.weg.oficinactw.application.dto.turma.TurmaRequestDTO;
 import com.weg.oficinactw.application.dto.turma.TurmaResponseDTO;
 import com.weg.oficinactw.application.mapper.TurmaMapper;
+import com.weg.oficinactw.domain.model.Turma;
+import com.weg.oficinactw.domain.model.Usuario;
+import com.weg.oficinactw.domain.model.enums.TipoUsuario;
 import com.weg.oficinactw.domain.service.TurmaService;
 import com.weg.oficinactw.infra.repository.TurmaRepositoryJPA;
+import com.weg.oficinactw.infra.repository.UsuarioRepositoryJPA;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,26 +18,58 @@ public class TurmaServiceImpl implements TurmaService {
 
     private final TurmaRepositoryJPA turmaRepositoryJPA;
     private final TurmaMapper turmaMapper;
+    private final UsuarioRepositoryJPA usuarioRepositoryJPA;
 
-    public TurmaServiceImpl(TurmaRepositoryJPA turmaRepositoryJPA, TurmaMapper turmaMapper) {
+    public TurmaServiceImpl(TurmaRepositoryJPA turmaRepositoryJPA, TurmaMapper turmaMapper, UsuarioRepositoryJPA usuarioRepositoryJPA) {
         this.turmaRepositoryJPA = turmaRepositoryJPA;
         this.turmaMapper = turmaMapper;
+        this.usuarioRepositoryJPA = usuarioRepositoryJPA;
     }
 
     @Override
     public TurmaResponseDTO criarTurma(TurmaRequestDTO turmaRequestDTO) {
+        try {
+            Turma turma = turmaMapper.requestToEntity(turmaRequestDTO);
 
+            turmaRepositoryJPA.save(turma);
 
-        return null;
+            return turmaMapper.requestToEntity(turma);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void matricularAluno(Long turmaId, Long alunoId) {
+        try {
+            Turma turma = turmaRepositoryJPA.findById(turmaId)
+                    .orElseThrow(() -> new RuntimeException("Erro ao buscar Turma por ID! "));
 
+            Usuario aluno = usuarioRepositoryJPA.findById(alunoId)
+                    .orElseThrow(() -> new RuntimeException("Erro ao buscar Aluno por ID! "));
+
+            if (!TipoUsuario.ALUNO.equals(aluno.getTipoUsuario())){
+                throw new RuntimeException("ID não remetente à um aluno! ");
+            }
+
+            aluno.setTurma(turma);
+
+            usuarioRepositoryJPA.save(aluno);
+
+
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<TurmaResponseDTO> listarTurmas() {
-        return List.of();
+        try {
+            return turmaRepositoryJPA.findAll().stream()
+                    .map(turmaMapper::requestToEntity)
+                    .toList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
