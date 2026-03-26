a97497b3c1f3f81efef90fe555fc36a0ad8f3f20
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-26 16:17:23 -0300

    feat: implementa os metodos da interface no UsuarioServiceImpl

--- oficinactw/src/main/java/com/weg/oficinactw/domain/service/UsuarioService.java
+++ oficinactw/src/main/java/com/weg/oficinactw/domain/service/UsuarioService.java
@@ -1,4 +1,15 @@
 package com.weg.oficinactw.domain.service;
 
+import com.weg.oficinactw.application.dto.usuario.UsuarioRequestDTO;
+import com.weg.oficinactw.application.dto.usuario.UsuarioResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
 public interface UsuarioService {
+    UsuarioResponseDTO criarUsuario (UsuarioRequestDTO usuarioRequestDTO);
+    UsuarioResponseDTO buscarPorId (Long id);
+    boolean validarProfessor(Long id);
+    List<UsuarioResponseDTO> listaAlunos();
 }
