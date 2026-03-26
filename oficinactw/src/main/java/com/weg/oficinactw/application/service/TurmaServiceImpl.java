687208169cb250abb9c4541d65c35f14be510a7b
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-26 16:25:40 -0300

    feat: implementa os metodos da interface no TurmaServiceImpl

--- oficinactw/src/main/java/com/weg/oficinactw/application/service/TurmaServiceImpl.java
+++ oficinactw/src/main/java/com/weg/oficinactw/application/service/TurmaServiceImpl.java
@@ -1,4 +1,26 @@
 package com.weg.oficinactw.application.service;
 
-public class TurmaServiceImpl {
+import com.weg.oficinactw.application.dto.turma.TurmaRequestDTO;
+import com.weg.oficinactw.application.dto.turma.TurmaResponseDTO;
+import com.weg.oficinactw.domain.service.TurmaService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TurmaServiceImpl implements TurmaService {
+    @Override
+    public TurmaResponseDTO criarTurma(TurmaRequestDTO turmaRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public void matricularAluno(Long turmaId, Long alunoId) {
+
+    }
+
+    @Override
+    public List<TurmaResponseDTO> listarAlunos() {
+        return List.of();
+    }
 }
--- oficinactw/src/main/java/com/weg/oficinactw/domain/service/TurmaService.java
+++ oficinactw/src/main/java/com/weg/oficinactw/domain/service/TurmaService.java
@@ -1,4 +1,12 @@
 package com.weg.oficinactw.domain.service;
 
+import com.weg.oficinactw.application.dto.turma.TurmaRequestDTO;
+import com.weg.oficinactw.application.dto.turma.TurmaResponseDTO;
+
+import java.util.List;
+
 public interface TurmaService {
+    TurmaResponseDTO criarTurma(TurmaRequestDTO turmaRequestDTO);
+    void matricularAluno (Long turmaId, Long alunoId);
+    List<TurmaResponseDTO> listarAlunos();
 }
