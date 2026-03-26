fdafa94a04648d4f73ec846e625c37b0dfc7dc1b
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-26 20:40:49 -0300

    fix: alteracao de String para TipoUsuario

--- oficinactw/src/main/java/com/weg/oficinactw/application/dto/usuario/UsuarioRequestDTO.java
+++ oficinactw/src/main/java/com/weg/oficinactw/application/dto/usuario/UsuarioRequestDTO.java
@@ -1,7 +1,9 @@
 package com.weg.oficinactw.application.dto.usuario;
 
+import com.weg.oficinactw.domain.model.enums.TipoUsuario;
+
 public record UsuarioRequestDTO(
         String nome,
-        String tipo
+        TipoUsuario tipo
 ) {
 }
