bd9e323aa817211a1bfbfd3b1f5e696b3aaf57da
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-26 20:40:26 -0300

    feat: criacao de metodos especificos de busca em UsuarioRepositoryJPA

--- oficinactw/src/main/java/com/weg/oficinactw/infra/repository/UsuarioRepositoryJPA.java
+++ oficinactw/src/main/java/com/weg/oficinactw/infra/repository/UsuarioRepositoryJPA.java
@@ -1,9 +1,14 @@
 package com.weg.oficinactw.infra.repository;
 
 import com.weg.oficinactw.domain.model.Usuario;
+import com.weg.oficinactw.domain.model.enums.TipoUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsuarioRepositoryJPA extends JpaRepository<Usuario, Long> {
+    Usuario findByProfessor(Long id);
+    List<Usuario> findByTipoUsuario(TipoUsuario tipoUsuario);
 }
