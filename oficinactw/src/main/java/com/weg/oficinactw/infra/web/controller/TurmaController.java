0714ba73fac4be0c5c9fa3d92cbaa8e31e8aff72
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-04-02 14:03:41 -0300

    feat: criacão do TurmaController

--- oficinactw/src/main/java/com/weg/oficinactw/infra/web/controller/TurmaController.java
+++ oficinactw/src/main/java/com/weg/oficinactw/infra/web/controller/TurmaController.java
@@ -1,4 +1,33 @@
 package com.weg.oficinactw.infra.web.controller;
 
+import com.weg.oficinactw.application.dto.turma.TurmaRequestDTO;
+import com.weg.oficinactw.application.dto.turma.TurmaResponseDTO;
+import com.weg.oficinactw.domain.service.TurmaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/turma")
 public class TurmaController {
+    private final TurmaService turmaService;
+
+    public TurmaController(TurmaService turmaService) {
+        this.turmaService = turmaService;
+    }
+
+    @PostMapping
+    public TurmaResponseDTO criarTurma(@RequestBody TurmaRequestDTO turmaRequestDTO) {
+        return turmaService.criarTurma(turmaRequestDTO);
+    }
+
+    @PostMapping("/{id}") // pega o id da turma na URL e o id do aluno no body
+    public void matricularAluno(@PathVariable Long turmaId, @RequestBody Long alunoId ) {
+        turmaService.matricularAluno(turmaId, alunoId);
+    }
+
+    @GetMapping
+    public List<TurmaResponseDTO> listarTurmas() {
+        return turmaService.listarTurmas();
+    }
 }
