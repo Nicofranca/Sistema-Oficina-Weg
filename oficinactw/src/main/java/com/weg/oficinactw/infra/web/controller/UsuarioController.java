d553d31b425c274ee4a6872bb2a054c664ed62d8
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-04-02 14:03:33 -0300

    feat: criacão do UsuarioController

--- oficinactw/src/main/java/com/weg/oficinactw/infra/web/controller/UsuarioController.java
+++ oficinactw/src/main/java/com/weg/oficinactw/infra/web/controller/UsuarioController.java
@@ -3,9 +3,7 @@ package com.weg.oficinactw.infra.web.controller;
 import com.weg.oficinactw.application.dto.usuario.UsuarioRequestDTO;
 import com.weg.oficinactw.application.dto.usuario.UsuarioResponseDTO;
 import com.weg.oficinactw.domain.service.UsuarioService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,21 +12,22 @@ import java.util.List;
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @GetMapping
     public List<UsuarioResponseDTO> listaAlunos() {
         return usuarioService.listaAlunos();
     }
 
     @GetMapping("/{id}")
-    public UsuarioResponseDTO buscarPorId(Long id) {
+    public UsuarioResponseDTO buscarPorId(@PathVariable Long id) {
         return usuarioService.buscarPorId(id);
     }
 
-    public UsuarioResponseDTO criarUsuario(UsuarioRequestDTO usuarioRequestDTO) {
+    @PostMapping
+    public UsuarioResponseDTO criarUsuario(@RequestBody UsuarioRequestDTO usuarioRequestDTO) {
         return usuarioService.criarUsuario(usuarioRequestDTO);
-    }
-
-    public UsuarioController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
     }
 }
