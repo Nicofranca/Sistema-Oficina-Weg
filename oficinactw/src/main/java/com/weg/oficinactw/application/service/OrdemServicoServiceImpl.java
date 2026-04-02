fd961eaef056b6da9433edd039da06c3535de263
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-04-02 14:53:18 -0300

    feat: adaptacao para os novos DTOs

--- oficinactw/src/main/java/com/weg/oficinactw/application/service/OrdemServicoServiceImpl.java
+++ oficinactw/src/main/java/com/weg/oficinactw/application/service/OrdemServicoServiceImpl.java
@@ -1,9 +1,13 @@
 package com.weg.oficinactw.application.service;
 
+import com.weg.oficinactw.application.dto.ordemServico.request.AberturaOrdemServicoRequestDTO;
+import com.weg.oficinactw.application.dto.ordemServico.request.ExecucaoOrdemServicoRequestDTO;
+import com.weg.oficinactw.application.dto.ordemServico.request.FinalizacaoOrdemServicoRequestDTO;
 import com.weg.oficinactw.application.dto.ordemServico.response.OrdemServicoResponseDTO;
 import com.weg.oficinactw.application.mapper.OrdemServicoMapper;
 import com.weg.oficinactw.domain.model.OrdemServico;
 import com.weg.oficinactw.domain.model.Usuario;
+import com.weg.oficinactw.domain.model.enums.StatusOrdemServico;
 import com.weg.oficinactw.domain.model.enums.TipoUsuario;
 import com.weg.oficinactw.domain.service.OrdemServicoService;
 import com.weg.oficinactw.infra.repository.OrdemServicoRepositoryJPA;
@@ -11,6 +15,7 @@ import com.weg.oficinactw.infra.repository.UsuarioRepositoryJPA;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrdemServicoServiceImpl implements OrdemServicoService {
@@ -25,28 +30,34 @@ public class OrdemServicoServiceImpl implements OrdemServicoService {
         this.usuarioRepositoryJPA = usuarioRepositoryJPA;
     }
 
-    @Override
-    public OrdemServicoResponseDTO abrirOrdemServico(Long professorId, List<Long> alunosId, String equipamento, String defeito) {
+    @Override //Long professorId, List<Long> alunosId, String equipamento, String defeito,
+    public OrdemServicoResponseDTO abrirOrdemServico(AberturaOrdemServicoRequestDTO aberturaOrdemServicoRequestDTO) {
         try {
 
-            Usuario usuario = usuarioRepositoryJPA.findById(professorId)
+            Usuario usuarioProfessor = usuarioRepositoryJPA.findById(aberturaOrdemServicoRequestDTO.professorId())
                     .orElseThrow(() -> new RuntimeException("Erro ao verificar usuario como professor! "));
 
-            if (!TipoUsuario.PROFESSOR.equals(usuario.getTipoUsuario())){
+            if (!TipoUsuario.PROFESSOR.equals(usuarioProfessor.getTipoUsuario())){
                 throw new IllegalArgumentException("Somente um professor pode abrir uma ordem de Servico! ");
             }
 
-            List<Usuario> listaAlunos = usuarioRepositoryJPA.findAllById(alunosId);
+            List<Usuario> listaAlunos = usuarioRepositoryJPA.findAllById(aberturaOrdemServicoRequestDTO.alunosId());
 
             if (listaAlunos.isEmpty()){
                 throw new IllegalArgumentException("É necessário pelo menos um aluno para abrir uma ordem! ");
             }
 
             OrdemServico ordemServico = new OrdemServico(
-
+                    listaAlunos,
+                    usuarioProfessor,
+                    StatusOrdemServico.ABERTA,
+                    aberturaOrdemServicoRequestDTO.equipamento(),
+                    aberturaOrdemServicoRequestDTO.defeitoRelatado()
             );
 
-            return null;
+            ordemServicoRepositoryJPA.save(ordemServico);
+
+            return ordemServicoMapper.requestToEntity(ordemServico);
 
         } catch (RuntimeException e) {
             throw new RuntimeException(e);
@@ -54,13 +65,38 @@ public class OrdemServicoServiceImpl implements OrdemServicoService {
     }
 
     @Override
-    public OrdemServicoResponseDTO registrarExecucao(Long ordemServicoId, Long alunoId, String materiais, String laudo) {
-        return null;
+    public OrdemServicoResponseDTO registrarExecucao(ExecucaoOrdemServicoRequestDTO execucaoOrdemServicoRequestDTO) {
+
+        List<Usuario> alunos = execucaoOrdemServicoRequestDTO.alunosId().stream()
+                .map(id -> usuarioRepositoryJPA.findById(id)
+                        .orElseThrow(() -> new RuntimeException("Aluno ID " + id + " não encontrado")))
+                .collect(Collectors.toList());
+
+        OrdemServico ordemServico = ordemServicoRepositoryJPA.findById(execucaoOrdemServicoRequestDTO.ordemId())
+                .orElseThrow(() -> new RuntimeException("Erro ao buscar Ordem Servico! "));
+
+        ordemServico.setAlunosEscalados(alunos);
+        ordemServico.setStatusOrdemServico(StatusOrdemServico.EXECUTANDO);
+        ordemServico.setMateriaisUsados(execucaoOrdemServicoRequestDTO.materiaisUsados());
+        ordemServico.setConclusaoTecnica(execucaoOrdemServicoRequestDTO.conclusao());
+
+        ordemServicoRepositoryJPA.save(ordemServico);
+
+        return ordemServicoMapper.requestToEntity(ordemServico);
     }
 
     @Override
-    public OrdemServicoResponseDTO finalizarOrdemServico(Long ordemServicoId, Long professorId) {
-        return null;
+    public OrdemServicoResponseDTO finalizarOrdemServico(FinalizacaoOrdemServicoRequestDTO finalizacaoOrdemServicoRequestDTO) {
+
+        OrdemServico ordemServico = ordemServicoRepositoryJPA.findById(finalizacaoOrdemServicoRequestDTO.ordemId())
+                .orElseThrow(() -> new RuntimeException("Erro ao buscar Ordem Servico! "));
+
+        Usuario usuarioProfessor = usuarioRepositoryJPA.findById(finalizacaoOrdemServicoRequestDTO.professorId())
+                .orElseThrow(() -> new RuntimeException("Erro ao buscar Professor! "));
+
+        ordemServico.setStatusOrdemServico(StatusOrdemServico.CONCLUIDA);
+
+        return ordemServicoMapper.requestToEntity(ordemServico);
     }
 
     @Override
--- oficinactw/src/main/java/com/weg/oficinactw/domain/service/OrdemServicoService.java
+++ oficinactw/src/main/java/com/weg/oficinactw/domain/service/OrdemServicoService.java
@@ -1,13 +1,16 @@
 package com.weg.oficinactw.domain.service;
 
+import com.weg.oficinactw.application.dto.ordemServico.request.AberturaOrdemServicoRequestDTO;
+import com.weg.oficinactw.application.dto.ordemServico.request.ExecucaoOrdemServicoRequestDTO;
+import com.weg.oficinactw.application.dto.ordemServico.request.FinalizacaoOrdemServicoRequestDTO;
 import com.weg.oficinactw.application.dto.ordemServico.response.OrdemServicoResponseDTO;
 
 import java.util.List;
 
 public interface OrdemServicoService {
-    OrdemServicoResponseDTO abrirOrdemServico (Long professorId, List<Long> alunosId, String equipamento, String defeito);
-    OrdemServicoResponseDTO registrarExecucao (Long ordemServicoId, Long alunoId, String materiais, String laudo);
-    OrdemServicoResponseDTO finalizarOrdemServico (Long ordemServicoId, Long professorId);
+    OrdemServicoResponseDTO abrirOrdemServico (AberturaOrdemServicoRequestDTO aberturaOrdemServicoRequestDTO);
+    OrdemServicoResponseDTO registrarExecucao (ExecucaoOrdemServicoRequestDTO execucaoOrdemServicoRequestDTO);
+    OrdemServicoResponseDTO finalizarOrdemServico (FinalizacaoOrdemServicoRequestDTO finalizacaoOrdemServicoRequestDTO);
     List<OrdemServicoResponseDTO> listarOSAtivas (); //Retorna apenas as que são concluidas
 }
 
